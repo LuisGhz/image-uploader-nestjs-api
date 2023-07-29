f54a0c4d04b20e542d3b074ff724014442de9677
Luis Gutiérrez <luisghtz@outlook.com>
2023-07-29 14:13:04 -0600

    Set npm completed path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/image-uploader-devchallenges-io
-npm install
-npm run build
+/opt/bitnami/node/bin/npm install
+/opt/bitnami/node/bin/npm run build
 cp /var/www/envs/image-uploader-devchallenges-io.env /var/www/image-uploader-devchallenges-io/dist/.env
 cd /var/www/image-uploader-devchallenges-io/dist/
 forever start main.js
