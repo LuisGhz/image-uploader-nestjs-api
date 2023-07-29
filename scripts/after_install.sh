12b183ab095809c73195bf1c570ecf77539d381b
Luis Gutiérrez <luisghtz@outlook.com>
2023-07-29 13:36:03 -0600

    Update scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "After install"
+cd /var/www/image-uploader-devchallenges-io
+npm install
+npm run build
+cp /var/www/envs/image-uploader-devchallenges-io.env /var/www/image-uploader-devchallenges-io/dist/.env
+cd /var/www/image-uploader-devchallenges-io/dist/
+forever start main.js
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "Before install"
+if [[ -d /var/www/image-uploader-devchallenges-io/dist ]] ; then
+  cd /var/www/image-uploader-devchallenges-io/dist
+  forever stop main.js
+fi
+cd /
+rm -rf /var/www/image-uploader-devchallenges-io
