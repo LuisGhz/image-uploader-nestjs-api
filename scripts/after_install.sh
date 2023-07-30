1f2625418d1bdab7540aa30a4adaa934d650a85e
Luis Gutiérrez <luisghtz@outlook.com>
2023-07-30 13:05:44 -0600

    Fix paths and user

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/bitnami/image-uploader-devchallenges-io
+cd /home/bitnami/www/image-uploader-devchallenges-io
 npm install
 npm run build
-cp /home/bitnami/envs/image-uploader-devchallenges-io.env /home/bitnami/image-uploader-devchallenges-io/dist/.env
-cd /home/bitnami/image-uploader-devchallenges-io/dist/
+cp /home/bitnami/www/envs/image-uploader-devchallenges-io.env /home/bitnami/www/image-uploader-devchallenges-io/dist/.env
+cd /home/bitnami/www/image-uploader-devchallenges-io/dist/
 forever start main.js
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [[ -d /home/bitnami/image-uploader-devchallenges-io/dist ]] ; then
-  cd /home/bitnami/image-uploader-devchallenges-io/dist
+if [[ -d /home/bitnami/www/image-uploader-devchallenges-io/dist ]] ; then
+  cd /home/bitnami/www/image-uploader-devchallenges-io/dist
   forever stop main.js
 fi
 cd /
-rm -rf /home/bitnami/image-uploader-devchallenges-io
+rm -rf /home/bitnami/www/image-uploader-devchallenges-io
